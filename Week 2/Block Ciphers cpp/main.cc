bff8815ff1e13fbf6bd475ce69256e822e139bb6
Kareem Shehata <kareem@shehata.ca>
2020-02-21 11:59:46 +1100

    Added a few more comments on why main is the way that it is

--- Week 2/Block Ciphers cpp/main.cc
+++ Week 2/Block Ciphers cpp/main.cc
@@ -1,5 +1,15 @@
 //  Created by Kareem Shehata on 2020-02-17.
 //  Copyright © 2020 Kareem Shehata. All rights reserved.
+//
+//  Example usage of block ciphers on command line.
+//  Gets mode as a command line argument, but then reads first line as key in
+//  hex format and then each line as ciphertext.
+//
+//  *** VERY IMPORTANT ***
+//  NEVER read keys or sensitive values on the command line!!
+//  Command line parameters can be read by other processes on your system and
+//  could leak your keys to another process or user!
+//  That's why this example reads the key from standard input.
 
 #include "block_cipher.h"
 #include "utils.h"
@@ -20,7 +30,7 @@ int main(int argc, const char * argv[]) {
   if (argc != 2) {
     cout << "Usage: " << argv[0] << " [mode]" << endl;
     cout << "Mode can be CBC or CTR" << endl;
-    cout << "Key is first input parsed from stdin, with CT read one per line"
+    cout << "Key is first line parsed from stdin, with CT read one per line"
       << endl;
     return 1;
   }
