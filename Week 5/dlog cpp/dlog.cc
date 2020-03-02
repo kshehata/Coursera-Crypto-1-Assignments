26cac6d494f3966acf928205f96a5d5fc2428444
Kareem Shehata <kareem@shehata.ca>
2020-03-02 23:00:57 +1100

    Use mpz numbers directly in hash table instead of converting to string.

--- Week 5/dlog cpp/dlog.cc
+++ Week 5/dlog cpp/dlog.cc
@@ -8,7 +8,14 @@ using std::string;
 using std::cout;
 using std::endl;
 
-constexpr int HASH_MAP_BASE = 10;
+namespace std {
+template <> struct hash<mpz_class> {
+  size_t operator()(const mpz_class& x) const {
+    return (x.get_mpz_t()[0]._mp_size == 0) ? 0 :
+      static_cast<size_t>(x.get_mpz_t()[0]._mp_d[0]);
+  }
+};
+}
 
 mpz_class exp_mod_n(const mpz_class& g, const mpz_class& x,
   const mpz_class& p) {
@@ -24,18 +31,16 @@ mpz_class dlog(const mpz_class& g, const mpz_class& h, const mpz_class& p,
 
   const long B = 1 << (n/2);
 
-  // TODO: mpz_class isn't hashable, need to find some other type in here that
-  // doesn't require conversions
-  std::unordered_map<string, long> left_half_table;
+  std::unordered_map<mpz_class, long> left_half_table;
   left_half_table.reserve(B);
 
   mpz_class l = h;
   mpz_class inv_g;
   mpz_invert(inv_g.get_mpz_t(), g.get_mpz_t(), p.get_mpz_t());
-  left_half_table[l.get_str(HASH_MAP_BASE)] = 0;
+  left_half_table[l] = 0;
   for (long x = 1; x <= B; ++x) {
     l = (l * inv_g) % p;
-    left_half_table[l.get_str(HASH_MAP_BASE)] = x;
+    left_half_table[l] = x;
   }
 
   // cout << "Hash Map:" << endl;
@@ -48,9 +53,9 @@ mpz_class dlog(const mpz_class& g, const mpz_class& h, const mpz_class& p,
   mpz_class r = base;
 
   for (long x = 0; x <= B; ++x) {
-    // cout << "Looking for value " << r.get_str(HASH_MAP_BASE) << " (" << x
+    // cout << "Looking for value " << r.get_str(10) << " (" << x
     //   << ") in hash map" << endl;
-    const auto it = left_half_table.find(r.get_str(HASH_MAP_BASE));
+    const auto it = left_half_table.find(r);
     if (it != left_half_table.end()) {
       return (x + 1) * B + it->second;
     }
