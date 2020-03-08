7bb5092bd0fbb5293b5e095072762563e1830133
Kareem Shehata <kareem@shehata.ca>
2020-03-08 12:42:57 +0700

    Added verification of hashes to main

--- Week 3/Video Hasher cpp/main.cc
+++ Week 3/Video Hasher cpp/main.cc
@@ -15,26 +15,49 @@ using std::ifstream;
 using std::ofstream;
 
 void usage(const string& name) {
-  cout << "Usage: " << name << " [input file] [output file]" << endl;
+  cout << "Usage:" << endl;
+  cout << "  Generate: " << name << " [input file] [output file]" << endl;
+  cout << "  Verify: " << name << " -V [input file] [first hash]" << endl;
   cout << "Calculates hashes for video file and inserts them in output" << endl;
 }
 
 int main(int argc, const char * argv[]) {
-  if (argc != 3) {
+  if (argc < 3 || argc > 4) {
     usage(argv[0]);
     return 1;
   }
-  ifstream input(argv[1], std::ios::binary);
-  if (!input) {
-    cout << "Could not open input file" << endl;
-    return 2;
-  }
-  ofstream output(argv[2], std::ios::binary);
-  if (!output) {
-    cout << "Could not open output file" << endl;
-    return 3;
-  }
 
-  cout << bytes2hex(hash_file(input, output)) << endl;
+  if (argc == 3) {
+    ifstream input(argv[1], std::ios::binary);
+    if (!input) {
+      cout << "Could not open input file" << endl;
+      return 2;
+    }
+    ofstream output(argv[2], std::ios::binary);
+    if (!output) {
+      cout << "Could not open output file" << endl;
+      return 3;
+    }
+
+    cout << bytes2hex(hash_file(input, output)) << endl;
+
+  } else if (argc == 4) {
+    if (string("-V").compare(argv[1])) {
+      usage(argv[0]);
+      return 1;
+    }
+
+    ifstream input(argv[2], std::ios::binary);
+    if (!input) {
+      cout << "Could not open input file" << endl;
+      return 2;
+    }
+    cout << "Verifying hashes..." << endl;
+    if (verify_hashes(input, hex2bytes(argv[3]))) {
+      cout << "All hashes correct" << endl;
+    } else {
+      cout << "VERIFICATION FAILED!" << endl;
+    }
+  }
   return 0;
 }
