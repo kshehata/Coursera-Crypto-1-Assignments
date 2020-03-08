e591a6a5a5aaa00fd028562dfe24a4ccff0a9c96
Kareem Shehata <kareem@shehata.ca>
2020-03-08 12:30:29 +0700

    Updated main to use the video hasher. Appears to hash correctly!

--- Week 3/Video Hasher cpp/main.cc
+++ Week 3/Video Hasher cpp/main.cc
@@ -4,16 +4,15 @@
 #include "video_hasher.h"
 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 using namespace video_hasher;
 using std::cin;
 using std::cout;
 using std::endl;
-using std::getline;
-using std::make_unique;
-using std::string;
-using std::unique_ptr;
+using std::ifstream;
+using std::ofstream;
 
 void usage(const string& name) {
   cout << "Usage: " << name << " [input file] [output file]" << endl;
@@ -23,7 +22,19 @@ void usage(const string& name) {
 int main(int argc, const char * argv[]) {
   if (argc != 3) {
     usage(argv[0]);
-    return -1;
+    return 1;
   }
+  ifstream input(argv[1], std::ios::binary);
+  if (!input) {
+    cout << "Could not open input file" << endl;
+    return 2;
+  }
+  ofstream output(argv[2], std::ios::binary);
+  if (!output) {
+    cout << "Could not open output file" << endl;
+    return 3;
+  }
+
+  cout << bytes2hex(hash_file(input, output)) << endl;
   return 0;
 }
